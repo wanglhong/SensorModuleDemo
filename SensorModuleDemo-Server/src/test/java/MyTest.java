b0efe7aa1b588281b2957fb61c94ec2dd99bd946
王立宏 <2198083211@qq.com>
2023-09-26 10:31:56 +0800

    测试配置

--- SensorModuleDemo-Server/src/test/java/MyTest.java
+++ SensorModuleDemo-Server/src/test/java/MyTest.java
@@ -1,30 +1,19 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 /**
- * Created by IntelliJ IDEA.
  *
+ * 描述:非SpringBoot自定义测试
  * @author 王立宏
- * 描述:
  * path: SensorModuleDemo-PACKAGE_NAME-MyTest
  * date: 2023/8/31 11:20
  */
 public class MyTest {
 
     @Test
-    public void mapTest() {
-        Map<String, Integer> map = new HashMap<>();
-        int a = (int) map.get("test001");
-        Integer b = Integer.valueOf(a) + 1;
-        map.put("test001", b);
-        Integer test001 = map.get("test001");
-    }
-
-    public static void main(String[] args) {
+    void test01() {
         List<String> dayList= new LinkedList<>();
         for (int i = 1; i <= 31; i++) {
             dayList.add("\"" + i + "号" + "\"");
