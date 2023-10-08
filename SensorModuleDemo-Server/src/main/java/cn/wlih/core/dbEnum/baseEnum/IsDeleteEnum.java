9d05ab90927cec770cdd5445e0e7216ac23db85f
王立宏 <2198083211@qq.com>
2023-10-08 10:11:48 +0800

    数据库枚举存储

--- SensorModuleDemo-Server/src/main/java/cn/wlih/core/dbEnum/baseEnum/IsDeleteEnum.java
+++ SensorModuleDemo-Server/src/main/java/cn/wlih/core/dbEnum/baseEnum/IsDeleteEnum.java
@@ -22,11 +22,15 @@ public enum IsDeleteEnum {
 
     /**
      * 数据库存储值
+     * @EnumValue 标记的枚举类属性的类型要和数据库字段的类型对应，否则在查询数据的时候无法转化为枚举类型，并显示为null
+     * 如果查询的时候，数据库字段的值匹配不到枚举，程序运行时并不会报错，而是显示为null；
      */
     @EnumValue
     private Integer key;
     /**
      * 前端显示值
+     * 在保存的时候，前端需要传递@JsonValue标记的枚举类属性的值，即"存在/删除"；
+     * 也可以传递枚举的索引值，0或"0"会转换成 EXIST。
      */
     @JsonValue
     private String display;
