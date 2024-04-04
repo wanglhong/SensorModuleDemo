d732d3565ee9f7f1d510d1f75f563dedd6e6b3a5
王立宏 <2198083211@qq.com>
2024-04-04 18:14:16 +0800

    feat(ALL): 海关审核添加关联的信息

--- SensorModuleDemo-Server/src/main/java/cn/wlih/app/vo/CustomsAuditVo.java
+++ SensorModuleDemo-Server/src/main/java/cn/wlih/app/vo/CustomsAuditVo.java
@@ -3,6 +3,9 @@ package cn.wlih.app.vo;
 import cn.wlih.app.model.modelDbEnum.AuditState;
 import cn.wlih.app.model.modelDbEnum.AuditType;
 import cn.wlih.core.base.vo.BaseModelVo;
+import cn.wlih.upms.vo.SysDeptVo;
+import cn.wlih.upms.vo.SysOrganizationVo;
+import cn.wlih.upms.vo.SysUserVo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -44,5 +47,29 @@ public class CustomsAuditVo extends BaseModelVo {
 
     @Schema(title = "审批人ID")
     private Long userIdOfAudit;
+
+    @Schema(title = "审批提交组织信息")
+    private SysOrganizationVo organizationOfSubmit;
+
+    @Schema(title = "审批提交部门信息")
+    private SysDeptVo deptOfSubmit;
+
+    @Schema(title = "审批提交人信息")
+    private SysUserVo userOfSubmit;
+
+    @Schema(title = "报关信息")
+    private CustomsDeclarationInfoVo customsDeclarationInfo;
+
+    @Schema(title = "清关信息")
+    private CustomsClearanceInfoVo customsClearanceInfo;
+
+    @Schema(title = "审批组织信息")
+    private SysOrganizationVo organizationOfAudit;
+
+    @Schema(title = "审批部门信息")
+    private SysDeptVo deptOfAudit;
+
+    @Schema(title = "审批人信息")
+    private SysUserVo userOfAudit;
 
 }
