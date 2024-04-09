da440df89063e318a25f6b73b201ec4965791a27
王立宏 <2198083211@qq.com>
2024-04-09 20:33:11 +0800

    feat(VideoInfoController): 添加视频监控查看占位接口

--- SensorModuleDemo-Server/src/main/java/cn/wlih/sensormodule/controller/VideoInfoController.java
+++ SensorModuleDemo-Server/src/main/java/cn/wlih/sensormodule/controller/VideoInfoController.java
@@ -1,13 +1,18 @@
 package cn.wlih.sensormodule.controller;
 
 import cn.wlih.core.base.controller.MyBaseController;
+import cn.wlih.core.base.model.ResponseResult;
+import cn.wlih.core.myAnnotate.MyRequestBody;
 import cn.wlih.sensormodule.dto.VideoInfoDto;
 import cn.wlih.sensormodule.model.VideoInfo;
 import cn.wlih.sensormodule.service.VideoInfoService;
 import cn.wlih.sensormodule.vo.VideoInfoVo;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,19 @@ public class VideoInfoController extends MyBaseController<VideoInfo, VideoInfoDt
 
     @Autowired
     private VideoInfoService videoInfoService;
+
+    /**
+     * 查看监控
+     */
+    @ApiOperationSupport(order = 6)
+    @Operation(summary = "查看监控")
+    @PostMapping("/viewMonitor")
+    public ResponseResult<String> viewMonitor(@MyRequestBody Long transportInfoId) {
+        if (transportInfoId == null) {
+            return ResponseResult.error("运输信息ID不能为空");
+        }
+        // TODO 获取监控地址
+        return ResponseResult.success("127.0.0.1:8084/hls/live_stream");
+    }
 
 }
